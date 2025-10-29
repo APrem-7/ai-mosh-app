65bb3bdd25b42c938a388c5784385b99c1db9dfd
Ananda Prem R <ap7leo@gmail.com>
2025-10-29 18:01:46 +0530

    configured vite in the frontend

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript + Express ");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
+
